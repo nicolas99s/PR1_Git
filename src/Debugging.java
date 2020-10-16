690868544b5827611a22bdda245f36d72779cbfa
nicot <nicot@LAPTOP-Q8TAQ8HK.local>
2020-10-16 17:20:11 +0200

    Initialize commit

---
+++ src/Debugging.java
@@ -0,0 +1,13 @@
+
+public class Debugging {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a;
+		a = 3;
+		System.out.println("1/a =" + 1/a);
+		a = 3.0/4;
+		System.out.println("1/a =" + 1/a);
+	}
+
+}
